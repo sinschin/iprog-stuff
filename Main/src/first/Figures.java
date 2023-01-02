1af65cfa9ae7824ff1508531d754f5e6df0f0612
sinschin <115551495+sinschin@users.noreply.github.com>
2023-01-02 12:53:34 +0100

    Added Comments

--- Main/src/first/Figures.java
+++ Main/src/first/Figures.java
@@ -16,6 +16,8 @@ public abstract class Figures implements Printable{
         return null;
     }
     //Variablen
-
+    int y,x, points;
     //Methoden
+    //Move
+
 }
--- Main/src/first/Pitch.java
+++ Main/src/first/Pitch.java
@@ -1,4 +1,14 @@
 package first;
 
+import java.util.ArrayList;
+
 public class Pitch {
+    //Variablen
+    ArrayList field = new ArrayList<>();
+
+    //Methoden
+    //GeneratePitch
+    //CreateFraction
+    //ShowPitch
+
 }
