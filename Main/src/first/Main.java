bda52fbeddfa92ab228699a5fb1a7eb6e239ba08
sinschin <115551495+sinschin@users.noreply.github.com>
2023-01-03 16:49:58 +0100

    Changed Field Design

--- Main/src/first/Main.java
+++ Main/src/first/Main.java
@@ -8,40 +8,42 @@ public class Main {
     }
     //Needs an option to show W/B and empty fields.
     public static void printField(){
-        //String fieldSpacer =      "|-------------------------------------------------|";
-        String fieldSpacer =      "##################################################################";
-        //String fieldSpacingLine = "|                                                 |";
-        //String fieldNormalLine =  "|  000   111   222   333   444   555   666   777  |";
         int numerator, denominator;
 
-        System.out.println(fieldSpacer);
+        String fieldSpacerStart  =      "┌───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┐";
+        String fieldSpacerMiddle =      "├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┤";
+        String fieldSpacerEnd    =      "└───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┘";
+
+
+        System.out.println(fieldSpacerStart);
         for (int i = 0; i < GameField.getSize(); i++) {
 
             //create row with numerator
-            System.out.print("#   ");
+            System.out.print("│  ");
             for (int j = 0; j < GameField.getSize(); j++) {
                 numerator =  GameField.getNumerator(i,j);
-                if(numerator < 100) { System.out.print(" " + numerator + "  #  ");}
-                else {System.out.print(numerator + "  #  ");}
+                if(numerator < 100) { System.out.print(" " + numerator + "  │  ");}
+                else {System.out.print(numerator + "  │  ");}
             }
             System.out.println();
 
             //create row with fraction separator
-            System.out.print("#   ");
+            System.out.print("│  ");
             for (int j = 0; j < GameField.getSize(); j++) {
-                System.out.print("———  #  ");
+                System.out.print("———  │  ");
             }
             System.out.println();
 
             //create row with denominator
-            System.out.print("#   ");
+            System.out.print("│  ");
             for (int j = 0; j < GameField.getSize(); j++) {
                 denominator =  GameField.getDenominator(i,j);
-                if(denominator < 100) { System.out.print(" " + denominator + "  #  ");}
-                else {System.out.print(denominator + "  #  ");}
+                if(denominator < 100) { System.out.print(" " + denominator + "  │  ");}
+                else {System.out.print(denominator + "  │  ");}
             }
             System.out.println();
-            System.out.println(fieldSpacer);
+            if (i < GameField.getSize() - 1) { System.out.println(fieldSpacerMiddle);}
+            else { System.out.println(fieldSpacerEnd);}
         }
     }
 }
