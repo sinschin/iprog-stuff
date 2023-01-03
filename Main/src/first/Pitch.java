45d6fa726761ce5a59bd67cc552121d25e019bb5
sinschin <115551495+sinschin@users.noreply.github.com>
2023-01-03 12:08:55 +0100

    Added Constructor, generatePitch, createFraction and gcd to Class Pitch

--- Main/src/first/Pitch.java
+++ Main/src/first/Pitch.java
@@ -1,14 +1,54 @@
 package first;
 
-import java.util.ArrayList;
+import javax.print.attribute.standard.PrinterMakeAndModel;
+import java.util.Random;
 
 public class Pitch {
-    //Variablen
-    ArrayList field = new ArrayList<>();
 
-    //Methoden
-    //GeneratePitch
-    //CreateFraction
-    //ShowPitch
+    //Variables
+    Fraction[][] field; //represents the playing field
+    int size; //indicates the size of the playing field
+
+    //Methods
+    public Pitch(int size) {
+        generatePitch(size);
+    }
+        //GeneratePitch
+    private void generatePitch(int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                field[i][j] = createFraction();
+            }
+        }
+    }
+    //creates suitable fractions according to the assignment
+    private Fraction createFraction() {
+        int numerator, denominator, gcdivisor;
+        Random random = new Random();
+
+        //create random number between 10 and 999
+        numerator = 10 + random.nextInt(989);
+        denominator = 10 + random.nextInt(989);
+
+        //truncate fraction
+        gcdivisor = gcd(numerator, denominator);
+        numerator = numerator / gcdivisor;
+        denominator = numerator / gcdivisor;
+
+        //check that numbers are at least in double digits
+        if (numerator < 9 || denominator < 9) {
+            return createFraction();
+        }
+        else {
+            return new Fraction(numerator, denominator);
+        }
+    }
+
+    //find the greatest common divisor
+    public int gcd(int a, int b) {
+        return (b == 0) ? a :gcd(b, a%b);
+    }
+        //showPitch
+        //setPlayers
 
 }
--- Main/src/first/Printable.java
+++ Main/src/first/Printable.java
@@ -3,5 +3,10 @@ package first;
 public interface Printable {
     int posX=Integer.MAX_VALUE;
     int posY=Integer.MAX_VALUE;
+
+    short printSizeX();
+
+    short printSizeY();
+
     String print();
 }
