d088a774fe2daf4c20dbfcd7b8152e0554c43399
sinschin <115551495+sinschin@users.noreply.github.com>
2023-01-04 11:22:04 +0100

    Added global usable variable size

--- Main/src/first/Main.java
+++ Main/src/first/Main.java
@@ -2,10 +2,13 @@ package first;
 
 public class Main {
     static Pitch GameField;
+    public static int size = 8; //size of the playing field, standard 8
     public static void main(String[] args) {
-        GameField = new Pitch(8);
+        GameField = new Pitch();
         printField();
     }
+
+    //Shows the playing field
     //Needs an option to show W/B and empty fields.
     public static void printField(){
         int numerator, denominator;
@@ -16,11 +19,11 @@ public class Main {
 
 
         System.out.println(fieldSpacerStart);
-        for (int i = 0; i < GameField.getSize(); i++) {
+        for (int i = 0; i < size; i++) {
 
             //create row with numerator
             System.out.print("│  ");
-            for (int j = 0; j < GameField.getSize(); j++) {
+            for (int j = 0; j < size; j++) {
                 numerator =  GameField.getNumerator(i,j);
                 if(numerator < 100) { System.out.print(" " + numerator + "  │  ");}
                 else {System.out.print(numerator + "  │  ");}
@@ -29,20 +32,20 @@ public class Main {
 
             //create row with fraction separator
             System.out.print("│  ");
-            for (int j = 0; j < GameField.getSize(); j++) {
+            for (int j = 0; j < size; j++) {
                 System.out.print("———  │  ");
             }
             System.out.println();
 
             //create row with denominator
             System.out.print("│  ");
-            for (int j = 0; j < GameField.getSize(); j++) {
+            for (int j = 0; j < size; j++) {
                 denominator =  GameField.getDenominator(i,j);
                 if(denominator < 100) { System.out.print(" " + denominator + "  │  ");}
                 else {System.out.print(denominator + "  │  ");}
             }
             System.out.println();
-            if (i < GameField.getSize() - 1) { System.out.println(fieldSpacerMiddle);}
+            if (i < size - 1) { System.out.println(fieldSpacerMiddle);}
             else { System.out.println(fieldSpacerEnd);}
         }
     }
